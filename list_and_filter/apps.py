1d605f9338d65197de99a4761362e97d75a2205c
Talha <talharizwan667@gmail.com>
2023-10-19 12:29:57 +0500

    updated apps.py

--- list_and_filter/apps.py
+++ list_and_filter/apps.py
@@ -11,3 +11,18 @@ class ListAndFilterConfig(AppConfig):
     """
 
     name = 'list_and_filter'
+    
+    plugin_app = {
+        "url_config": {
+            "lms.djangoapp": {
+                "namespace": "list_and_filter",
+                "regex": r"^list",
+                'relative_path': 'urls',
+            },
+            "cms.djangoapp": {
+                "namespace": "list_and_filter",
+                "regex": r"^list",
+                'relative_path': 'urls',
+            }
+        },
+}
